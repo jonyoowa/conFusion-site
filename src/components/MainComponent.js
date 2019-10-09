a15e9806f9abb0ced402742547f86dfcb912cbbe
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2019-10-09 16:44:19 -0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,10 +23,6 @@ class Main extends Component {
         };
     }
 
-    // onDishSelect(dishId) {
-    //     this.setState({ selectedDish: dishId});
-    // }
-
     render() {
         const HomePage = () => {
             return (
@@ -38,12 +34,22 @@ class Main extends Component {
             );
         }
 
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail 
+                    dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
+                />
+            );
+        };      
+
         return (
             <div>
                 <Header />
                     <Switch>
                         <Route path='/home' component={HomePage} />
                         <Route exact path='/menu' render={() => <Menu dishes={this.state.dishes} />} />
+                        <Route path='/menu/:dishId' component={DishWithId} />
                         <Route exact path='/contactus' component={Contact} />
                         <Redirect to="/home" />
                     </Switch>
