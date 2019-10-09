8e4e84c5f621c2260a08955242f5a071bdff1b11
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2019-10-09 16:35:58 -0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,12 +6,20 @@ import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -22,7 +30,11 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}  
+                />
             );
         }
 
@@ -32,6 +44,7 @@ class Main extends Component {
                     <Switch>
                         <Route path='/home' component={HomePage} />
                         <Route exact path='/menu' render={() => <Menu dishes={this.state.dishes} />} />
+                        <Route exact path='/contactus' component={Contact} />
                         <Redirect to="/home" />
                     </Switch>
                 <Footer />
