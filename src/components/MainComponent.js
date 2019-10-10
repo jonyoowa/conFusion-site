67fd5581f54378a9ada172633cae10b9ad111ef5
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2019-10-10 02:51:55 -0700

    Redux Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
-
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
-
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -27,25 +20,26 @@ const mapStateToProps = state => {
 }   
 
 const mapDispatchToProps = dispatch => ({
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes())}
 });
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            promotions: PROMOTIONS,
-            leaders: LEADERS
-        };
     }   
+
+    componentDidMount() {
+        this.props.fetchDishes();
+    }  
 
     render() {
         const HomePage = () => {
-            return (
+            return(
                 <Home 
-                    dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
                     promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
                     leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
@@ -55,28 +49,29 @@ class Main extends Component {
         const AboutUs = () => {
             return (
                 <About 
-                    leaders={this.state.leaders}  
+                    leaders={this.props.leaders}  
                 />
             )
         }
 
         const DishWithId = ({match}) => {
-            return (
-                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            return(
+                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                            isLoading={this.props.dishes.isLoading}
+                            errMess={this.props.dishes.errMess}
                             comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
                             addComment={this.props.addComment}
                 />
             );
-        };      
+        };
 
         return (
             <div>
                 <Header />
                     <Switch>
                         <Route path='/home' component={HomePage} />
-                        {/* <Route path ='/aboutus' component={AboutUs} /> */}
                         <Route exact path='/aboutus' render={() => <AboutUs leaders={this.props.leaders} />} />
-                        <Route exact path='/menu' render={() => <Menu dishes={this.state.dishes} />} />
+                        <Route exact path='/menu' render={() => <Menu dishes={this.props.dishes} />} />
                         <Route path='/menu/:dishId' component={DishWithId} />
                         <Route exact path='/contactus' component={Contact} />
                         <Redirect to="/home" />
